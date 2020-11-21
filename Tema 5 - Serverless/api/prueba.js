65212552aba7673a9b93aa4c6297722e8f277c0b
Diego Hernandez Moreno <diegohm1598@gmail.com>
2020-11-21 18:07:30 +0100

    cambiado prueba.js

--- Tema 5 - Serverless/api/prueba.js
+++ Tema 5 - Serverless/api/prueba.js
@@ -9,6 +9,6 @@
 
 
 module.exports = (req, res) => {
-		const nombre = {nombre = "Desconocido"} = req.query
+		const {nombre = "Desconocido"} = req.query
 		req.status(200).send("Hola, " + nombre + "!")
 }
