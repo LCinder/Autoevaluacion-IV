5fddff826b4db1afe14913f451ae377be0af1bd9
Diego Hernandez Moreno <diegohm1598@gmail.com>
2020-11-21 18:01:36 +0100

    netlfily.toml incluido

--- Tema 5 - Serverless/api/prueba.js
+++ Tema 5 - Serverless/api/prueba.js
@@ -1,14 +1,14 @@
-
-exports.handler = async (event) => {
-	return {
-		statusCode: 200,
-		body: "Funcionaaaa"
-	}
-}
-
-
 //
-// module.exports = (req, res) => {
-// 		const nombre = {nombre = "Desconocido"} = req.query
-// 		req.status(200).send("Hola, " + nombre + "!")
+// exports.handler = async (event) => {
+// 	return {
+// 		statusCode: 200,
+// 		body: "Funcionaaaa"
+// 	}
 // }
+
+
+
+module.exports = (req, res) => {
+		const nombre = {nombre = "Desconocido"} = req.query
+		req.status(200).send("Hola, " + nombre + "!")
+}
